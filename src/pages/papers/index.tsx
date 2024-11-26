a3038800dfe1217ecff4eb616a9a77c74923f35d
ddang8-jpeg <parkrangerdang@gmail.com>
2024-11-26 14:33:49 -0500

    Added margins between gird and header.

--- src/pages/papers/index.tsx
+++ src/pages/papers/index.tsx
@@ -12,7 +12,7 @@ const PapersPage: React.FC<PageProps> = () => {
       <div className="flex flex-col w-full mx-auto">
         <Nav activePage="papers" />
         <Header title={'Papers'} />
-        <div className="relative max-w-6xl w-full mx-auto mt-2 mb-12">
+        <div className="relative max-w-6xl w-full mx-auto mt-4 mb-12">
           <div className="md:mx-8 bg-lightBlue-300 py-10 px-2 md:px-6 rounded-md md:shadow-lg shadow-slate-400">
             <div className="flex-col max-w-2xl"></div>
             <PapersGrid />
